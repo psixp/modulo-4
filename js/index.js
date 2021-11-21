0d26a8170b6b9771cd39d59773f737adfa1526df
psixp <pablo_six@live.com>
2021-11-21 10:37:49 -0300

    atualização de botão do modo dark com conceitos mais simples.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
-const $html = document.querySelector('html')
+/*const $html = document.querySelector('html')
 				const $checkbox = document.querySelector('#switch')
 
 				$checkbox.addEventListener('change', function(){
 					$html.classList.toggle('dark-mode')})
+*/
+
+const toggle = document.getElementById('toggle');
+const body = document.querySelector('body')
+const a = document.querySelector('a')
+
+toggle.onclick = function () {
+	toggle.classList.toggle('active');
+	body.classList.toggle('active');
+	a.classList.toggle('active')
+}
